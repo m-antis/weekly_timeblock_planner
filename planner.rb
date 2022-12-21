1d7933e25ca4d5631c674ca7e73de75b4af880ca
hyunggilwoo <hyunggilswoo@gmail.com>
2022-12-21 15:43:51 -0800

    Refactored to generate Sunday
    What is next:
    Fix the bugs to return the date of a Sunday

--- planner.rb
+++ planner.rb
@@ -34,7 +34,7 @@ FONTS = {
 #  week = ...
 # name = name + "_" + quarter + "_" + week + ".pdf"
 # 
-def generate_file_name
+def generate_file_name()
   file_name = "time_block_pages"
   file_name += 
   file_name += ".pdf"
@@ -468,8 +468,8 @@ def weekend_page saturday, sunday
 end
 
 # TODO: refactoring the sunday method, to reformat file name
-def generate_sunday
-  sunday = if ARGV.empty?
+def generate_sunday input_date
+  sunday = if input_date.empty?
     date = DateTime.now.to_date
     if date.wday > 2
       puts "Generating pages for the next week"
@@ -479,7 +479,7 @@ def generate_sunday
       date.prev_day(date.wday)
     end
   else
-    date = DateTime.parse(ARGV.first).to_date
+    date = DateTime.parse(input_date.first).to_date
     puts "Parsed #{date} from arguments"
     date.prev_day(date.wday)
   end
@@ -493,20 +493,7 @@ Prawn::Document.generate(FILE_NAME, margin: RIGHT_PAGE_MARGINS, print_scaling: :
   stroke_color MEDIUM_COLOR
   line_width(0.5)
 
-  sunday = if ARGV.empty?
-      date = DateTime.now.to_date
-      if date.wday > 2
-        puts "Generating pages for the next week"
-        date.next_day(7-date.wday)
-      else
-        puts "Generating pages for this week"
-        date.prev_day(date.wday)
-      end
-    else
-      date = DateTime.parse(ARGV.first).to_date
-      puts "Parsed #{date} from arguments"
-      date.prev_day(date.wday)
-    end
+  sunday = generate_sunday(ARGV)
 
   WEEKS.times do |week|
     unless week.zero?
